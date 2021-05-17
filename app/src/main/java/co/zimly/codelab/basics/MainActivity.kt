f80cfab4107dc087505da3a80c24eb87c3f929e6
David Bailey <davidbailey00@outlook.com>
2021-05-17 17:51:09 +0100

    Animate tapping on text

--- app/src/main/java/co/zimly/codelab/basics/MainActivity.kt
+++ app/src/main/java/co/zimly/codelab/basics/MainActivity.kt
@@ -3,6 +3,9 @@ package co.zimly.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
@@ -10,6 +13,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -40,13 +45,13 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun MyScreenContent(names: List<String> = List(1000) { "Android #$it" }) {
-    val counterState = remember { mutableStateOf(0) }
+    var counterState by remember { mutableStateOf(0) }
 
     Column(Modifier.fillMaxHeight()) {
         NameList(names, Modifier.weight(1f))
         Counter(
-            count = counterState.value,
-            updateCount = { newCount -> counterState.value = newCount }
+            count = counterState,
+            updateCount = { newCount -> counterState = newCount }
         )
     }
 }
@@ -63,7 +68,18 @@ fun NameList(names: List<String>, modifier: Modifier = Modifier) {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(
+        if (isSelected) Color.Red else Color.Transparent
+    )
+
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .padding(24.dp)
+            .background(color = backgroundColor)
+            .clickable(onClick = { isSelected = !isSelected })
+    )
 }
 
 @Composable
