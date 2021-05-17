5a0532cde009fe5d96737f6e07ab57b92276d5c5
David Bailey <davidbailey00@outlook.com>
2021-05-17 18:06:11 +0100

    reformat all files

--- app/src/main/java/co/zimly/codelab/basics/MainActivity.kt
+++ app/src/main/java/co/zimly/codelab/basics/MainActivity.kt
@@ -12,11 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
